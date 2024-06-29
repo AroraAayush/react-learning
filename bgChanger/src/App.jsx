9668be1e245d33178628b1348ba6959384171681
Aayush Arora <80632973+AroraAayush@users.noreply.github.com>
2024-06-29 08:11:33 +0530

    Background changer in React Project

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [color,setColor]=useState("white")
+  const changeBackground=(color)=>{
+    const innerFunc=()=>{
+      setColor(color);
+    }
+    return innerFunc;
+  }
+  return (
+    <>
+    <div className="w-full h-screen" style={{backgroundColor:color}}>    
+    <div className="box" >
+      <h3>Background Changer</h3>
+      <button className="btn" id="orange" onClick={changeBackground("orange")}>Orange</button>
+      <button className="btn" id="green" onClick={changeBackground("green")}>Green</button>
+      <button className="btn" id="blue" onClick={changeBackground("blue")}>Blue</button>
+      <button className="btn" id="red" onClick={changeBackground("red")}>Red</button>
+      <button className="btn" id="pink" onClick={changeBackground("pink")}>Pink</button>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
